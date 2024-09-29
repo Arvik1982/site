01d74427fc42071cfa9cc4c2cba1642ae8ecbdd5
Arseny <phone.kulikovarseniy@gmail.com>
2024-09-29 17:40:30 +0300

    fix

--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -1,43 +1,44 @@
-'use client'
+"use client";
+import styles from "./header.module.css";
+import { useState } from "react";
+import LogoTop from "../Logo/Logo";
 
-import Image from 'next/image'
-import styles from'./header.module.css'
-import { useState } from 'react';
-import LogoTop from '../Logo/Logo';
+export default function Header() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-export default function Header(){
-
-    const [isMenuOpen, setIsMenuOpen] = useState(false);
-
-    const toggleMenu = () => {
-        setIsMenuOpen(!isMenuOpen);
-    };
-    return(
-        <header className={styles.header__container}>
-            <div className={styles.header__container_logo}>
-                <LogoTop/>
-            </div>
-            <div className={styles.header__container_contacts}>
-               <span> mail</span>
-               <span> tg</span>
-            </div>
-            <nav className={styles.header__container_nav}>
-                <button onClick={toggleMenu} className={styles.burgerButton}>
-                    {/* Иконка бургер-меню */}
-                    <span className={styles.burgerLine}></span>
-                    <span className={styles.burgerLine}></span>
-                    <span className={styles.burgerLine}></span>
-                    
-                </button>
-                
-            </nav>
-            {isMenuOpen && (
-                    <ul className={`${styles.navList} ${styles.slideIn}`}>
-                        <li><a href="/">Главная</a></li>
-                        <li><a href="/about">О нас</a></li>
-                        <li><a href="/contact">Контакты</a></li>
-                    </ul>
-                )}
-        </header>
-    )
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+  return (
+    <header className={styles.header__container}>
+      <div className={styles.header__container_logo}>
+        <LogoTop />
+      </div>
+      <div className={styles.header__container_contacts}>
+        <span> mail</span>
+        <span> tg</span>
+      </div>
+      <nav className={styles.header__container_nav}>
+        <button onClick={toggleMenu} className={styles.burgerButton}>
+          {/* Иконка бургер-меню */}
+          <span className={styles.burgerLine}></span>
+          <span className={styles.burgerLine}></span>
+          <span className={styles.burgerLine}></span>
+        </button>
+      </nav>
+      {isMenuOpen && (
+        <ul className={`${styles.navList} ${styles.slideIn}`}>
+          <li>
+            <a href="/">Главная</a>
+          </li>
+          <li>
+            <a href="/about">О нас</a>
+          </li>
+          <li>
+            <a href="/contact">Контакты</a>
+          </li>
+        </ul>
+      )}
+    </header>
+  );
 }
