33239239feddc574fb60d3baad8ebe8d7aed02e3
Arseny <phone.kulikovarseniy@gmail.com>
2024-09-29 22:32:22 +0300

    logo-resize

--- app/components/Logo/Logo.tsx
+++ app/components/Logo/Logo.tsx
@@ -1,8 +1,8 @@
 export default function LogoTop() {
   return (
     <svg
-      width="60px"
-      height="60px"
+      width="80px"
+      height="80px"
       viewBox="0 0 64 64"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
