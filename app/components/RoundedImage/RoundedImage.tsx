e115d57e7cf4b98abde56dbb29ec72b118379919
Arseny <phone.kulikovarseniy@gmail.com>
2024-10-08 20:28:41 +0300

    fix

--- app/components/RoundedImage/RoundedImage.tsx
+++ app/components/RoundedImage/RoundedImage.tsx
@@ -1,25 +1,44 @@
 import Image, { StaticImageData } from "next/image";
-
 import styles from "./roundedImage.module.css";
+import RedPhoneIcon from "../Icons/RedPhoneIcon";
+import RedTelegramIcon from "../Icons/RedTelegramIcon";
+import RedEnvelopeIcon from "../Icons/RedEnvelopeIcon";
 
-type TProps={
-   img?: StaticImageData
-}
+type TProps = {
+  img?: StaticImageData;
+};
 
-export default function RoundedImage({img}:TProps){
+export default function RoundedImage({ img }: TProps) {
+  return (
+    <div className={styles.container__picture}>
+      <picture className={styles.about__info_avatar}>
+        {img && (
+          <Image className={styles.info__avatar_img} src={img} alt="avatar" />
+        )}
+        {!img && <span></span>}
+      </picture>
 
-    return(
-        <div className={styles.container__picture}>
-        <picture className={styles.about__info_avatar}>
-          {img && (
-            <Image
-              className={styles.info__avatar_img}
-              src={img}
-              alt="avatar"
-            />
-          )}
-          {!img && <span></span>}
-        </picture>
-      </div>
-    )
+      <a
+        href="tel:89217553973"
+        style={{ top: "18px", left: "-3px" }}
+        className={styles.container__picture_button}
+      >
+        <RedPhoneIcon />
+      </a>
+      <a
+        href="tg://resolve?domain=kulikovarseniy"
+        style={{ top: "72px", left: "-33px" }}
+        className={styles.container__picture_button}
+      >
+        <RedTelegramIcon />
+      </a>
+      <a
+        href="mailto:mail@mail.com"
+        style={{ top: "133px", left: "-20px" }}
+        className={styles.container__picture_button}
+      >
+        <RedEnvelopeIcon />
+      </a>
+    </div>
+  );
 }
